1af6edc2d7e54bebce48019c8494008cdcd43b30
keinernarvaez <ckeinercano@gmail.com>
2025-04-25 00:36:42 -0500

    Avances, seguridad

--- OAuth-2/src/main/java/com/example/Auth2/config/SecurityConfig.java
+++ OAuth-2/src/main/java/com/example/Auth2/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import com.example.Auth2.service.customOauth2UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,23 +14,32 @@ import org.springframework.beans.factory.annotation.Autowired;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-@Autowired
-private customOauth2UserService customOAuth2UserService;
-@Bean
-public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-    return httpSecurity
-        .authorizeHttpRequests(request -> {
-            request.requestMatchers(HttpMethod.GET, "/", "/hello","/api/v1/user/").permitAll();
-            request.anyRequest().authenticated();
-        })
-        .formLogin(Customizer.withDefaults())
-        .oauth2Login(oauth2 -> oauth2
-            .userInfoEndpoint(userInfo -> userInfo
-                .userService(customOAuth2UserService)
-            )
-            .defaultSuccessUrl("http://127.0.0.1:5500/user.html", true)
-        )
-        .build();
-}
+    @Autowired
+    private customOauth2UserService customOAuth2UserService;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+
+        return httpSecurity
+        .csrf(csrf -> csrf.disable())
+        .authorizeHttpRequests(
+                        autRequest -> autRequest
+                                        .requestMatchers("/api/v1/user/").permitAll()
+                                        .requestMatchers("/hello").permitAll()
+                                        .requestMatchers("/oauth2/authorization/**").permitAll()
+                                        .requestMatchers("/login/oauth2/code/**").permitAll()
+                                        .requestMatchers("/oauth2/authorization/**").permitAll()
+                                        .anyRequest().authenticated())
+                .sessionManagement(sessionManagement -> sessionManagement
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                // .formLogin(Customizer.withDefaults())
+                // .oauth2Login(oauth2 -> oauth2
+                // .userInfoEndpoint(userInfo -> userInfo
+                // .userService(customOAuth2UserService)
+                // )
+                // .defaultSuccessUrl("http://127.0.0.1:5500/user.html", true)
+                // )
+                .build();
+    }
 
 }
